459093fcee4518f21ee4b2240f0f488437c91c9c
nOttOD4y <kaungmyat97k@gmail.com>
2022-09-05 19:53:22 +0630

    90% donegit status

---
+++ src/main/java/com/test/dao/CourseDao.java
@@ -0,0 +1,27 @@
+package com.test.dao;
+
+import java.util.List;
+
+import com.test.dto.CourseReq;
+import com.test.dto.CourseRes;
+
+public interface CourseDao {
+
+	
+	  void insertCourse(CourseRes res);
+	
+	  int couresMaxid();
+	  
+	  List<CourseRes> showCouse();
+	
+	  List<CourseRes> getcourseofone(Integer i);
+	  
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/test/dao/LoginLogoutDao.java
@@ -0,0 +1,16 @@
+package com.test.dao;
+
+import com.test.dto.UserReq;
+import com.test.dto.UserRes;
+
+public interface LoginLogoutDao {
+
+
+			UserRes login(UserRes req);
+ 
+
+
+
+
+
+}
